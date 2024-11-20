f5f59bd86f9480ce2691c35385b3495f2e47ca3e
Valerii <valerapiliaev@gmail.com>
2024-11-20 11:09:45 +0200

    Added api for cart item, compatison, order, product, product category, wishlist

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,109 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    DestroyAPIView,
+    ListAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+)
 
-# Create your views here.
+from api.serializers import (
+    CartItemSerializer,
+    ComparisonSerializer,
+    OrderSerializer,
+    ProductCategorySerializer,
+    ProductSerializer,
+    WishlistSerializer,
+)
+from mygadget.models import (
+    CartItem,
+    Comparison,
+    Order,
+    Product,
+    ProductCategory,
+    Wishlist,
+)
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateView(CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductUpdateView(UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDeleteView(DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCategoryListView(ListAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class ProductCategoryDetailView(RetrieveAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class ProductCategoryCreateView(CreateAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class ProductCategoryUpdateView(UpdateAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class ProductCategoryDeleteView(DestroyAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = ProductCategorySerializer
+
+
+class CartItemListView(ListAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class CartItemCreateView(CreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class CartItemUpdateView(UpdateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class CartItemDeleteView(DestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+
+class WishlistListView(ListAPIView):
+    queryset = Wishlist.objects.all()
+    serializer_class = WishlistSerializer
+
+
+class ComparisonListView(ListAPIView):
+    queryset = Comparison.objects.all()
+    serializer_class = ComparisonSerializer
+
+
+class OrderListView(ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
